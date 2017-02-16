4903124ab6161654fa5ada1b7246641f6dca66a9
Rohan Allison <rohanthewiz_at_gmail.com>
2017-02-15 23:47:42 -0600

    WIP adding restful routes

--- main.go
+++ main.go
@@ -11,10 +11,18 @@ func main() {
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome"))
+		w.Write([]byte("Welcome to the Home page"))
 	})
+	r.Get("/ping", func(w http.ResponseWriter, r * http.Request) {
+		w.Write([]byte("pong"))
+	})
+	r.Get("/panic", func(w http.ResponseWriter, r * http.Request) {
+		panic("Testing panic")
+	})
+
 
 	http.ListenAndServe(":3000", r)
 }
