e7f269c3d52c3242aaef911003bf1e6276f36a0e
Rohan Allison <rohanthewiz_at_gmail.com>
2017-02-16 01:23:35 -0600

    WIP adding restful routes

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"net/http"
 	"github.com/pressly/chi"
 	"github.com/pressly/chi/middleware"
+	"math/rand"
+	"fmt"
+	"errors"
+	"github.com/pressly/chi/render"
+	"context"
 )
 
 func main() {
@@ -23,6 +28,164 @@ func main() {
 		panic("Testing panic")
 	})
 
+	// Restful routes for Articles resource
+	r.Route("/articles", func(r chi.Router) {
+		r.With(paginate).Get("/", ListArticles)
+		r.Post("/", CreateArticle)			// POST /articles
+		r.Get("/search", SearchArticles)  // GET /articles/search
+
+		r.Route("/:articleID", func(r chi.Router) {
+			r.Use(ArticleCtx)   // Load the Article on the request context
+			r.Get("/", GetArticle)  // GET /articles/123
+			r.Put("/", UpdateArticle)  // PUT /articles/123
+			r.Delete("/", DeleteArticle) // DELETE /arcticles/123
+		})
+	})
+
 
 	http.ListenAndServe(":3000", r)
+}
+
+type Article struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+}
+
+// Article fixture
+var articles = []*Article {
+	{ID: "1", Title: "Hi"},
+	{ID: "2", Title: "sup"},
+}
+
+func ArticleCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc( func(w http.ResponseWriter, r *http.Request) {
+		articleID := chi.URLParam(r, "arcticleID")
+		article, err := dbGetArticle(articleID)
+		if err != nil {
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, http.StatusText(http.StatusNotFound))
+			return
+		}
+		ctx := context.WithValue(r.Context(), "article", article)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// SearchArticles searches the Articles data for a matching article.
+// It's just a stub, but you get the idea.
+func SearchArticles(w http.ResponseWriter, r *http.Request) {
+	// Filter by query param, and search...
+	render.JSON(w, r, articles)
+}
+
+// ListArticles returns an array of Articles.
+func ListArticles(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, articles)
+}
+
+// CreateArticle persists the posted Article and returns it
+// back to the client as an acknowledgement.
+func CreateArticle(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		*Article
+		OmitID interface{} `json:"id,omitempty"`  // prevents 'id' from being set
+	}
+	// The above is a nifty trick for how to omit fields during JSON unmarshalling
+
+	if err := render.Bind(r.Body, &data); err != nil {
+		render.JSON(w, r, err.Error());  return
+	}
+
+	article := data.Article
+	dbNewArticle(article)
+
+	render.JSON(w, r, article)
+}
+
+// GetArticle returns the specific Article. You'll notice it just
+// fetches the Article right off the context, as its understood that
+// if we made it this far, the Article must be on the context. In case
+// its not due to a bug, then it will panic, and our Recoverer will save us.
+func GetArticle(w http.ResponseWriter, r *http.Request) {
+	// Assume if we've reach this far, we can access the article
+	// context because this handler is a child of the ArticleCtx
+	// middleware. The worst case, the recoverer middleware will save us.
+	article := r.Context().Value("article").(*Article)
+
+	// chi provides a basic companion subpackage "github.com/pressly/chi/render", however
+	// you can use any responder compatible with net/http.
+	render.JSON(w, r, article)
+}
+
+
+// UpdateArticle updates an existing Article in our persistent store.
+func UpdateArticle(w http.ResponseWriter, r *http.Request) {
+	article := r.Context().Value("article").(*Article)
+
+	data := struct {
+		*Article
+		OmitID interface{} `json:"id,omitempty"` // prevents 'id' from being overridden
+	}{Article: article}
+
+	if err := render.Bind(r.Body, &data); err != nil {
+		render.JSON(w, r, err)
+		return
+	}
+	article = data.Article
+
+	render.JSON(w, r, article)
+}
+
+// DeleteArticle removes an existing Article from our persistent store.
+func DeleteArticle(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	// Assume if we've reach this far, we can access the article
+	// context because this handler is a child of the ArticleCtx
+	// middleware. The worst case, the recoverer middleware will save us.
+	article := r.Context().Value("article").(*Article)
+
+	article, err = dbRemoveArticle(article.ID)
+	if err != nil {
+		render.JSON(w, r, err)
+		return
+	}
+
+	// Respond with the deleted object, up to you.
+	render.JSON(w, r, article)
+}
+
+
+func paginate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// just a stub.. some ideas are to look at URL query params for something like
+		// the page number, or the limit, and send a query cursor down the chain
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Mock a data storage
+func dbNewArticle(article *Article) (string, error) {
+	article.ID = fmt.Sprintf("%d", rand.Intn(100) + 10)
+	articles = append(articles, article)
+	return article.ID, nil
+}
+
+func dbGetArticle(id string) (*Article, error) {
+	for _, a := range articles {
+		if a.ID == id {
+			return a, nil
+		}
+	}
+	return nil, errors.New("article not found.")
+}
+
+func dbRemoveArticle(id string) (*Article, error) {
+	for i, a := range articles {
+		if a.ID == id {
+			articles = append((articles)[:i], (articles)[i+1:]...)
+			return a, nil
+		}
+	}
+	return nil, errors.New("article not found.")
 }
